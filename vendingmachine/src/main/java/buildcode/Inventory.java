faa6c12bae4ded29626774db49d21d19203bcf0e
BarshaBits <161912217+BarshaBits@users.noreply.github.com>
2025-03-18 01:05:18 +0530

    first commit

---
+++ vendingmachine/src/main/java/buildcode/Inventory.java
@@ -0,0 +1,23 @@
+package buildcode;
+
+import buildcode.model.Product;
+
+import java.util.List;
+
+public class Inventory {
+private  int noOfProducts;
+private final List<Product> products;
+
+public Inventory(List<Product> products)
+{
+  this.products=products;
+}
+
+public void addProduct(int productId, int rackId) {
+
+}
+
+public void removeProduct(int productId, int rackId) {
+
+}
+}
---
+++ vendingmachine/src/main/java/buildcode/model/Product.java
@@ -0,0 +1,7 @@
+package buildcode.model;
+
+public class Product {
+    private String name;
+    private String id;
+    private double price;
+}
